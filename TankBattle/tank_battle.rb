117bdcc07d3097b8b3ff9ad1a92025306c097667
Rabya Tayal <69662484+RDalie@users.noreply.github.com>
2021-06-02 16:38:57 +0530

    Add files via upload

--- TankBattle/tank_battle.rb
+++ TankBattle/tank_battle.rb
@@ -99,16 +99,27 @@ ENEMY_FREQUENCY = 0.008
 
     class Enemy
         attr_accessor :x, :y, :radius, :speed, :image, :angle
-        def initialize(x,y)
+        def initialize(x,y,speed)
             @x = x 
             @y = y
-            @speed = 2
+            @speed = speed
             @radius = 30
             @angle = 0
             @image = Gosu::Image.new('Sprites/images/aliens3.png')
 
         end
 
+    end
+
+# HOURGLASS CLASS
+
+    class Hourglass
+        attr_accessor :x, :y, :image
+        def initialize(x,y)
+            @image = Gosu::Image.new('Sprites/images/hourglass.png')
+            @x = x
+            @y = y 
+        end
     end
 
 # ----------------------------------------FUNCTIONS-----------------------------------------
@@ -209,14 +220,28 @@ class TankBattle < Gosu::Window
         self.caption = 'Tank Battle'
 
         @player = Player.new()
-        
+        @enemy_speed = 3
         # Arrays to keep track of sprites
-        @enemies = [Enemy.new(0,0)]
+        @enemies = [Enemy.new(0,0,@enemy_speed)]
         @bullets = []   
-        @enemies_killed = 0  
+        @enemies_killed = 0 
+        @hourglasses = [] 
 
         # Font
         @font30 = Gosu::Font.new(30)
+        @font60 = Gosu::Font.new(60)
+
+        # true if it's hourglass mode
+        @hourglass_on = false
+        @hourglass_image = Gosu::Image.new('Sprites/images/hourglass.png')
+        
+        # scenes
+        @game_over = false
+        @game_over_scene = Gosu::Image.new('game_over.png')
+
+        #timer
+        @clock_image = Gosu::Image.new('clock.png')
+        @game_start_time = Gosu.milliseconds
 
 
     end
@@ -231,96 +256,187 @@ class TankBattle < Gosu::Window
             @bullets.push(Bullet.new(x, y, @player.angle))
         end
 
-    
+        # lets the player try again once the game is over
+        if (@game_over)
+            if id == Gosu::MS_LEFT 
+                if mouse_x > 500 and mouse_x < 700 and mouse_y > 495 and mouse_y < 520
+                    #reinitialize the arrays
+                    @game_over = false
+                    @enemies.clear()
+                    @player.x = 300
+                    @player.y = 400
+                    @time_elapsed = 0
+                    @game_start_time = Gosu.milliseconds
+                    @enemies_killed = 0
+                    @hourglasses.clear()
+                    @hourglass_on = false
+                end
+            end
+        end
     end
 
     def update()
 
-        # MOVEMENT
-        # Rotating the tank on button press
-        player_turn_right(@player) if button_down?(Gosu::KB_RIGHT)
-        player_turn_left(@player) if button_down?(Gosu::KB_LEFT)
 
 
-        # moving the tank forward
-        player_accelerate(@player) if button_down?(Gosu::KB_UP)
-        player_move(@player)
+        if (!@game_over)
 
-        # moving the bullets
-        @bullets.each do |bullet|
-            bullet_move(bullet)
-        end
+            @time_elapsed = (Gosu.milliseconds - @game_start_time)/1000
+        
+            # MOVEMENT
+            # Rotating the tank on button press
+            player_turn_right(@player) if button_down?(Gosu::KB_RIGHT)
+            player_turn_left(@player) if button_down?(Gosu::KB_LEFT)
 
-        # ADDING A NEW ENEMY DEPENDING ON A CERTAIN FREQUENCY
-        if rand < ENEMY_FREQUENCY
 
-            if rand < 0.5
-                enemy = Enemy.new(0, rand * SCREEN_HEIGHT)  
-            else 
-                enemy = Enemy.new(rand * SCREEN_WIDTH, 0)  
+            # moving the tank forward
+            player_accelerate(@player) if button_down?(Gosu::KB_UP)
+            player_move(@player)
+
+            # moving the bullets
+            @bullets.each do |bullet|
+                bullet_move(bullet)
             end
 
-            @enemies.push(enemy)   
-        end
+            # ADDING A NEW ENEMY DEPENDING ON A CERTAIN FREQUENCY
+            if rand < ENEMY_FREQUENCY
 
-        # making the enemy move
-        @enemies.each do |enemy|
-            angle = enemy_direction(@player,enemy)
-            enemy_move(enemy, angle)
-        end
+                if rand < 0.5
+                    enemy = Enemy.new(0, rand * SCREEN_HEIGHT, @enemy_speed)  
+                else 
+                    enemy = Enemy.new(rand * SCREEN_WIDTH, 0, @enemy_speed)  
+                end
 
-        # COLLISIONS
-        # detecting collision between the bullets and the enemies
-        @enemies.dup.each do |enemy|
-            @bullets.dup.each do |bullet|
-                distance = Gosu.distance(enemy.x, enemy.y, bullet.x, bullet.y)
-                if distance < enemy.radius + bullet.radius + 5 # 5 is some buffer to hit enemy easily
-                    @enemies_killed += 1
+                @enemies.push(enemy)   
+            end
 
-                    # deleting the enemy and the bullet after collision and adding an explosion
+            # ADDING A NEW HOUR GLASS DEPENDING ON CERTAIN FREQUENCY
+            if rand < 0.001
+                hourglass = Hourglass.new(rand * SCREEN_WIDTH, rand * SCREEN_HEIGHT)
+                @hourglasses.push(hourglass)
+            end
+
+            # making the enemy move
+            @enemies.each do |enemy|
+                angle = enemy_direction(@player,enemy)
+                enemy_move(enemy, angle)
+            end
+
+            # COLLISIONS
+            # detecting collision between the bullets and the enemies
+            @enemies.dup.each do |enemy|
+                @bullets.dup.each do |bullet|
+                    distance = Gosu.distance(enemy.x, enemy.y, bullet.x, bullet.y)
+                    if distance < enemy.radius + bullet.radius + 5 # 5 is some buffer to hit enemy easily
+                        @enemies_killed += 1
+
+                        # deleting the enemy and the bullet after collision and adding an explosion
+                        @enemies.delete(enemy)
+                        @bullets.delete(bullet)
+                    end    
+                end
+
+            end
+
+            @hourglasses.dup.each do |hourglass|
+                distance = Gosu.distance(hourglass.x, hourglass.y, @player.x, @player.y)
+                if distance < @player.radius + 5
+                    @hourglasses.delete(hourglass)
+                    @hourglass_on = true
+                    @hourglass_time = Gosu.milliseconds
+                end
+            end
+
+            if (@hourglass_on)
+                @enemies.each do |enemy|
+                    enemy.speed = 2
+                end
+            else
+                @enemies.each do |enemy|
+                    enemy.speed = 4
+                end
+            end
+
+            if (@hourglass_on)
+                @enemy_speed = 1
+                
+                if ((Gosu.milliseconds - @hourglass_time) > 10000)
+                    @hourglass_on = false
+                    @enemy_speed = 2
+                end
+            end
+
+            # CLEANING THE ARRAYS
+            # deleting the enemies which are out of the window
+            @enemies.dup.each do |enemy|
+                if enemy.y > SCREEN_HEIGHT + enemy.radius
                     @enemies.delete(enemy)
-                    @bullets.delete(bullet)
-                end    
+                end
+            end
+
+            # deleting the bullets which are not onscreen
+            @bullets.dup.each do |bullet|
+                @bullets.delete(bullet) unless onscreen?(bullet.radius, bullet.x, bullet.y)
             end
 
         end
 
-        # CLEANING THE ARRAYS
-        # deleting the enemies which are out of the window
+        # GAME OVER
         @enemies.dup.each do |enemy|
-            if enemy.y > SCREEN_HEIGHT + enemy.radius
-                @enemies.delete(enemy)
+            distance = Gosu.distance(enemy.x, enemy.y, @player.x, @player.y)
+            if distance < enemy.radius + @player.radius 
+                @game_over = true
+                @game_over_message = "KILLED BY AN ENEMY"
+
             end
         end
 
-        # deleting the bullets which are not onscreen
-        @bullets.dup.each do |bullet|
-            @bullets.delete(bullet) unless onscreen?(bullet.radius, bullet.x, bullet.y)
+        if (@time_elapsed == 200)
+            @game_over = true
+            @game_over_message = "------TIME IS UP------"
         end
-
+        
     end
 
     def draw()
 
-        # BACKGROUND
-        draw_rect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,Gosu::Color.argb(0xff_808080),ZOrder::BACKGROUND, mode = :default)
+        if (!@game_over)
+            # BACKGROUND
+            draw_rect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT,Gosu::Color.argb(0xff_808080),ZOrder::BACKGROUND, mode = :default)
 
-        # PLAYER
-        @player.image.draw_rot(@player.x, @player.y, ZOrder::PLAYER, @player.angle)
+            # PLAYER
+            @player.image.draw_rot(@player.x, @player.y, ZOrder::PLAYER, @player.angle)
 
-        # BULLETS
-        @bullets.each do |bullet|
-            bullet.image.draw_rot(bullet.x , bullet.y , ZOrder::BULLET, bullet.angle)
+            # BULLETS
+            @bullets.each do |bullet|
+                bullet.image.draw_rot(bullet.x , bullet.y , ZOrder::BULLET, bullet.angle)
+            end
+
+            @hourglasses.each do |hourglass|
+                hourglass.image.draw(hourglass.x, hourglass.y, ZOrder::PLAYER)
+            end
+
+            # ENEMY
+            @enemies.each do |enemy|
+                enemy.image.draw_rot(enemy.x, enemy.y, ZOrder::PLAYER, enemy.angle)
+            end
+
+            #SCORE
+            @font30.draw_text("Enemies killed: #{@enemies_killed}", 0,0, 2, 1,1, 0xff_000000)
+            if (@hourglass_on)
+                @font30.draw_text("#{(10 - (Gosu.milliseconds - @hourglass_time)/1000)}",SCREEN_WIDTH-80, 20, 1)
+                @hourglass_image.draw(SCREEN_WIDTH-120, 20)
+            end
+
+            #TIMER
+            @clock_image.draw(SCREEN_WIDTH-100, SCREEN_HEIGHT-100, ZOrder::TOP, scale_x = 0.2, scale_y = 0.2)
+            @font30.draw_text("#{@time_elapsed}", SCREEN_WIDTH-50, SCREEN_HEIGHT - 80, ZOrder::TOP, 1, 1,0xff_000000)
+
+        else
+            @game_over_scene.draw(0,0,ZOrder::TOP)
+            @font60.draw_text("Score: #{@enemies_killed}", 500, 80, ZOrder::TOP, 1, 1, 0xff_ffffff )
+            @font30.draw_text("#{@game_over_message}", 480, 190, ZOrder::TOP, 1, 1, 0xff_ff0000 )
         end
-
-        # ENEMY
-        @enemies.each do |enemy|
-            enemy.image.draw_rot(enemy.x, enemy.y, ZOrder::PLAYER, enemy.angle)
-        end
-
-        #SCORE
-        @font30.draw_text("Enemies killed: #{@enemies_killed}", 0,0, 2, 1,1, 0xff_000000)
-
     end
 end
 
